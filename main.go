fc6041697600288b2b6b89c7eb97971e0c38cb3f
Kshitij Dhyani <wimpywarlord@gmail.com>
2020-10-18 14:54:21 +0530

    add hot reload nodemon and gitignore

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-    "net/http"
-    "encoding/json"
+	"net/http"
 )
+
+// GLOBAL VARIABLE
+var Meetings []Meeting
 
 type Meeting struct {
 	Id                 string `json:"Id"`
@@ -22,24 +25,28 @@ type Participant struct {
 	RSVP  string `json:"RSVP"`
 }
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+// func homePage(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Welcome to the HomePage!")
+// 	fmt.Println("Endpoint Hit: homePage")
+// }
+
+// END POINT FOR "GET A MEETING USING ID"
+func returnMeetingOfId(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint For: Listing Meeting of id X")
+	json.NewEncoder(w).Encode(Meetings)
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", returnMeetingOfId)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-
-
 func main() {
-var Meetings []Meeting;
-    Meetings = []Meeting{
-        Meeting{Id:"1",Title:"KSHITIJ IS GOING TO GET SELECTED",Participants:"2",Start_Time:"123",End_Time:"456",Creation_Timestamp:"789"},
-        Meeting{Id:"2",Title:"KSHITIJ IS BEST CANDIDATE",Participants:"2",Start_Time:"123",End_Time:"456",Creation_Timestamp:"789"},
-        Meeting{Id:"3",Title:"KSHITIJ WILL WORK HARD",Participants:"2",Start_Time:"123",End_Time:"456",Creation_Timestamp:"789"},
-    }
+
+	Meetings = []Meeting{
+		Meeting{Id: "1", Title: "KSHITIJ IS GOING TO GET SELECTED", Participants: "2", Start_Time: "123", End_Time: "456", Creation_Timestamp: "789"},
+		Meeting{Id: "2", Title: "KSHITIJ IS BEST CANDIDATE", Participants: "2", Start_Time: "123", End_Time: "456", Creation_Timestamp: "789"},
+		Meeting{Id: "3", Title: "KSHITIJ WILL WORK HARD", Participants: "2", Start_Time: "123", End_Time: "456", Creation_Timestamp: "789"},
+	}
 	handleRequests()
 }
